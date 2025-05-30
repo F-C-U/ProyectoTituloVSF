06149b2d2d8f4fdad7f4cf3697c123c4e3967687
CETECOM Concepción <fr.castillou@duocuc.cl>
2025-05-30 09:42:02 -0400

    Fix #46

--- src/app/services/firebase.service.ts
+++ src/app/services/firebase.service.ts
@@ -1,5 +1,6 @@
 import { Injectable, inject } from '@angular/core';
 import { AngularFirestore } from '@angular/fire/compat/firestore';
+import { setDoc, doc, getFirestore, getDoc, updateDoc, deleteDoc } from '@angular/fire/firestore';
 import { getAuth, signInWithEmailAndPassword, createUserWithEmailAndPassword, sendPasswordResetEmail, updateProfile } from '@angular/fire/auth';
 import { User } from '../models/user.model';
 @Injectable({
@@ -12,22 +13,45 @@ export class FirebaseService {
   signIn(user: User) {
     return signInWithEmailAndPassword(getAuth(), user.email, user.password)
   }
+
   signUp(user: User) {
     return createUserWithEmailAndPassword(getAuth(), user.email, user.password)
-  } 
+  }
+
   signOut() {
     return getAuth().signOut();
   }
+
   updateUser(displayName: string) {
-  const user = getAuth().currentUser;
-  if (user) {
-    return updateProfile(user, { displayName });
-  } else {
-    return Promise.reject(new Error('No user is currently signed in.'));
-  }
+    const user = getAuth().currentUser;
+    if (user) {
+      return updateProfile(user, { displayName });
+    } else {
+      return Promise.reject(new Error('No user is currently signed in.'));
+    }
   }
   sendRecoveryEmail(email: string) {
     return sendPasswordResetEmail(getAuth(), email)
+  }
+
+  setDocument(path: string, data: any) {
+    return setDoc(doc(getFirestore(), path), data);
+  }
+
+  async getDocument(path: string) {
+    return (await getDoc(doc(getFirestore(), path))).data();
+  }
+
+  updateDocument(path: string, data: any) {
+    return updateDoc(doc(getFirestore(), path), data);
+  }
+
+  deleteDocument(path: string) {
+    return deleteDoc(doc(getFirestore(), path));
+  }
+
+  getCurrentUser() {
+    return getAuth().currentUser;
   }
 }
 
