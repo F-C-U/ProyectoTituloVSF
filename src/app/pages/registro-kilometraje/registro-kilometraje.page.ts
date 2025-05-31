82a2a1e449d672532dbf12944b97af87fcd1fc11
Francisco Castillo Urra <franciscocastillourra@gmail.com>
2025-05-31 16:54:37 -0400

    closes #8

--- src/app/pages/registro-kilometraje/registro-kilometraje.page.ts
+++ src/app/pages/registro-kilometraje/registro-kilometraje.page.ts
@@ -1,23 +1,30 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule, ReactiveFormsModule, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import {
+  FormsModule,
+  ReactiveFormsModule,
+  FormBuilder,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
+import { FirebaseService } from 'src/app/services/firebase.service';
 
 @Component({
   selector: 'app-registro-kilometraje',
   standalone: true,
   imports: [CommonModule, IonicModule, FormsModule, ReactiveFormsModule],
   templateUrl: './registro-kilometraje.page.html',
-  styleUrls: ['./registro-kilometraje.page.scss']
+  styleUrls: ['./registro-kilometraje.page.scss'],
 })
 export class RegistroKilometrajePage {
   formularioKilometraje: FormGroup;
 
-  constructor(private fb: FormBuilder) {
+  constructor(private fb: FormBuilder, private firebase: FirebaseService) {
     // Inicializamos el formulario con validaciones
     this.formularioKilometraje = this.fb.group({
       fecha: ['', Validators.required],
-      kilometros: ['', [Validators.required, Validators.min(0)]]
+      kilometros: ['', [Validators.required, Validators.min(0)]],
     });
   }
 
@@ -29,6 +36,12 @@ export class RegistroKilometrajePage {
     if (this.formularioKilometraje.valid) {
       console.log('Kilometraje registrado:', this.formularioKilometraje.value);
       // Aquí podrías enviar los datos a una API o servicio
+      try {
+        this.firebase.setDocument(
+          'kilometraje/',
+          this.formularioKilometraje.value
+        );
+      } catch (error) {}
     } else {
       this.formularioKilometraje.markAllAsTouched();
     }
