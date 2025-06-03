617f76e55bd8fba7b0a7d1d2207e5e6094ccc9e1
Sebastián Apileo <se.apileo@duocuc.cl>
2025-06-03 13:07:17 -0400

    Implementar interfaz de ganancias de conductores. Close #32

--- src/app/pages/ganancias/ganancias.page.ts
+++ src/app/pages/ganancias/ganancias.page.ts
@@ -1,20 +1,53 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import {
+  IonContent,
+  IonHeader,
+  IonTitle,
+  IonToolbar,
+  IonCard,
+  IonCardHeader,
+  IonCardTitle,
+  IonCardContent,
+  IonItem,
+  IonLabel,
+  IonNote
+} from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-ganancias',
   templateUrl: './ganancias.page.html',
   styleUrls: ['./ganancias.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [
+    CommonModule,
+    FormsModule,
+    IonContent,
+    IonHeader,
+    IonTitle,
+    IonToolbar,
+    IonCard,
+    IonCardHeader,
+    IonCardTitle,
+    IonCardContent,
+    IonItem,
+    IonLabel,
+    IonNote
+  ]
 })
 export class GananciasPage implements OnInit {
+  conductor: string = 'Vito Bozzano';
+
+  gananciaBruta: number = 100000;
+  deduccionEsquema: number = 20000;
+  deduccionCombustible: number = 15000;
+
+  get gananciaNeta(): number {
+    return this.gananciaBruta - this.deduccionEsquema - this.deduccionCombustible;
+  }
 
   constructor() { }
 
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
