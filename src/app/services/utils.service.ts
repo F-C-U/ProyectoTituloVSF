5f5ba96b0b30833dd02525baf0411e71cbf7de95
Francisco Castillo Urra <franciscocastillourra@gmail.com>
2025-06-10 17:12:36 -0400

    Agregado router link

--- src/app/services/utils.service.ts
+++ src/app/services/utils.service.ts
@@ -1,14 +1,22 @@
-import { Injectable } from '@angular/core';
+import { inject, Injectable } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root',
 })
+
 export class UtilsService {
+  router = inject(Router);
+
   saveInLocalStorage(key: string, value: any) {
     localStorage.setItem(key, JSON.stringify(value));
   }
 
   getFromlocalStorage(key: string) {
     return JSON.parse(localStorage.getItem(key) || 'null');
+  }
+
+  routerLink(url:string){
+    return this.router.navigate([url]);
   }
 }
