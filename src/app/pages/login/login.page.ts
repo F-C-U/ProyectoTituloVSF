d6eaf2a3b54e170d6646eb978cb63bb13042713d
Francisco Castillo Urra <franciscocastillourra@gmail.com>
2025-06-12 16:31:42 -0400

    modificada la ruta de inicio por login

--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -1,6 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormBuilder, FormGroup, Validators, ReactiveFormsModule, FormsModule } from '@angular/forms';
+import {
+  FormBuilder,
+  FormGroup,
+  Validators,
+  ReactiveFormsModule,
+  FormsModule,
+} from '@angular/forms';
 import { IonicModule, ToastController } from '@ionic/angular';
 import { FirebaseService } from 'src/app/services/firebase.service';
 import { User } from 'src/app/models/user.model';
@@ -11,21 +17,26 @@ import { UtilsService } from 'src/app/services/utils.service';
   standalone: true,
   imports: [CommonModule, FormsModule, ReactiveFormsModule, IonicModule],
   templateUrl: './login.page.html',
-  styleUrls: ['./login.page.scss']
+  styleUrls: ['./login.page.scss'],
 })
 export class LoginPage {
   formularioLogin: FormGroup;
 
-  constructor(private fb: FormBuilder, private toastController: ToastController,private firebase:FirebaseService,private utils:UtilsService) {
+  constructor(
+    private fb: FormBuilder,
+    private toastController: ToastController,
+    private firebase: FirebaseService,
+    private utils: UtilsService
+  ) {
     // Creamos el formulario con validaciones necesarias
     this.formularioLogin = this.fb.group({
       correo: ['', [Validators.required, Validators.email]],
-      contrasena: ['', [Validators.required]]
+      contrasena: ['', [Validators.required]],
     });
   }
 
-  ngOninit(){
-    if(this.utils.getFromlocalStorage('usuario')!==null){
+  ngOninit() {
+    if (this.utils.getFromlocalStorage('usuario') !== null) {
       this.utils.routerLink('/home');
     }
   }
@@ -35,41 +46,35 @@ export class LoginPage {
       message: mensaje,
       duration: 2500,
       color,
-      position: 'bottom'
+      position: 'bottom',
     });
     toast.present();
   }
 
   // Método para manejar el envío del formulario
   onSubmit() {
-    if (this.formularioLogin.invalid) {
-      this.mostrarMensaje('Por favor completa todos los campos correctamente.', 'danger');
-      return;
+    if (this.formularioLogin.valid) {
+      this.firebase.signIn(this.formularioLogin.value as User).then((res) => {
+        this.getUserInfo(res.user.uid);
+      });
     }
-
-    const { correo, contrasena } = this.formularioLogin.value;
-    // Aquí podrías llamar a un servicio de autenticación
-    this.firebase.signIn(this.formularioLogin.value as User).then(res => {
-      this.getUserInfo(res.user.uid);
-    })
-    // Aquí se realizaría la lógica de autenticación (ej. llamada HTTP al backend)
-    console.log('Intento de login:', correo, contrasena);
-    this.mostrarMensaje('Inicio de sesión exitoso.');
-    this.formularioLogin.reset();
   }
 
   async getUserInfo(uid: string) {
-    if (this.formularioLogin.valid){
-      let path = `usuarios/${uid}`;	
+    if (this.formularioLogin.valid) {
+      let path = `usuarios/${uid}`;
       await this.firebase.getDocument(path).then((user: any | null) => {
         if (user) {
           this.utils.saveInLocalStorage('usuario', user);
           this.utils.routerLink('home');
           this.formularioLogin.reset();
         } else {
-          this.mostrarMensaje('No se encontró información del usuario.', 'danger');
+          this.mostrarMensaje(
+            'No se encontró información del usuario.',
+            'danger'
+          );
         }
-      })
+      });
     }
   }
 }
