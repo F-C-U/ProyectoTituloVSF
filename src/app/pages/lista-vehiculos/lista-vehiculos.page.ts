679e7419ca5e212e06cfce94a002a67c7514d635
Francisco Castillo Urra <franciscocastillourra@gmail.com>
2025-06-11 20:07:16 -0400

    closes #6

--- src/app/pages/lista-vehiculos/lista-vehiculos.page.ts
+++ src/app/pages/lista-vehiculos/lista-vehiculos.page.ts
@@ -10,33 +10,38 @@ import { UtilsService } from 'src/app/services/utils.service';
   standalone: true,
   imports: [CommonModule, IonicModule, RouterModule],
   templateUrl: './lista-vehiculos.page.html',
-  styleUrls: ['./lista-vehiculos.page.scss']
+  styleUrls: ['./lista-vehiculos.page.scss'],
 })
 export class ListaVehiculosPage {
   cargando = signal(true); // Estado de carga de la vista
   toastCtrl = inject(ToastController);
 
-  constructor(private firebase:FirebaseService,private utils:UtilsService) {
+  constructor(private firebase: FirebaseService, private utils: UtilsService) {
     this.obtenerVehiculos();
   }
-  registroVehiculos : { patente:string , modelo:string, anio:number,tipoCombustible:string}[] = []
+  registroVehiculos: {
+    patente: string;
+    modelo: string;
+    anio: number;
+    tipoCombustible: string;
+  }[] = [];
 
   obtenerVehiculos() {
-      this.firebase.getCollection('vehiculos').subscribe((data:any[]) => {
-        this.registroVehiculos = data.map(vehiculo => ({
-          patente: vehiculo.patente,
-          modelo: vehiculo.modelo,
-          anio: vehiculo.anio,
-          tipoCombustible: vehiculo.tipoCombustible
-        }));
-      })
-    }
+    this.firebase.getCollection('vehiculos').subscribe((data: any[]) => {
+      this.registroVehiculos = data.map((vehiculo) => ({
+        patente: vehiculo.patente,
+        modelo: vehiculo.modelo,
+        anio: vehiculo.anio,
+        tipoCombustible: vehiculo.tipoCombustible,
+      }));
+    });
+  }
 
   async mostrarError(mensaje: string) {
     const toast = await this.toastCtrl.create({
       message: mensaje,
       duration: 3000,
-      color: 'danger'
+      color: 'danger',
     });
     toast.present();
   }
@@ -44,9 +49,12 @@ export class ListaVehiculosPage {
   editarVehiculo(vehiculoId: string) {
     let xtras: NavigationExtras = {
       state: {
-        vehiculoId: vehiculoId
-      }
-    }
+        vehiculoId: vehiculoId,
+      },
+    };
     this.utils.routerLinkWithExtras('editar-vehiculo', xtras);
   }
-} 
+  eliminarVehiculo(vehiculoId: string) {
+    this.firebase.deleteDocument('vehiculos/' + vehiculoId);
+  }
+}
