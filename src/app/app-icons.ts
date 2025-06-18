16183ac45d293cd4f62581701c4fa656e1766203
Sebastián Apileo <se.apileo@duocuc.cl>
2025-06-18 09:38:11 -0400

    Agregado .ts para añadir iconos. Fixed #61

---
+++ src/app/app-icons.ts
@@ -0,0 +1,21 @@
+// src/app/app-icons.ts
+import { addIcons } from 'ionicons';
+import {
+    carSportOutline,
+    speedometerOutline,
+    flashOutline,
+    constructOutline,
+    cashOutline,
+    logOutOutline
+} from 'ionicons/icons';
+
+export const initializeIcons = () => {
+    addIcons({
+        carSportOutline,
+        speedometerOutline,
+        flashOutline,
+        constructOutline,
+        cashOutline,
+        logOutOutline
+    });
+};
--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,8 @@ import { provideFirebaseApp,initializeApp } from '@angular/fire/app';
 import { provideAuth, getAuth } from '@angular/fire/auth';
 import { provideFirestore, getFirestore } from '@angular/fire/firestore';
 import { environment } from './environments/environment';
+import { initializeIcons } from './app/app-icons';
+initializeIcons();
 bootstrapApplication(AppComponent, {
   providers: [
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
